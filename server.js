323bc4e2e751bb3d3d5e8c13b801fef6e75e1d7b
Onatesla <73800811+Onatesla@users.noreply.github.com>
2022-03-18 16:01:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.set('port', process.env.PORT || 1337);
 app.use(express.json())
 app.use(express.urlencoded())
 //routes
-app.post("/posta", (req,res)=> {
-   const  nome  = req.body.logo
-  console.log(nome)
-   if (!nome){
-     res.status(418).send({message: "Rammi stu nomi mongolo"})
-   }
-   res.send({ message: `io odio ${nome} e mi deve pure soldi` })
-})
+// app.post("/posta", (req,res)=> {
+//    const  nome  = req.body.logo
+//   console.log(nome)
+//    if (!nome){
+//      res.status(418).send({message: "Rammi stu nomi mongolo"})
+//    }
+//    res.send({ message: `io odio ${nome} e mi deve pure soldi` })
+// })
 
 app.get("/", (req,res)=>{
   res.send("idle...")
