15f0e1dd32bed78a0fbc8cd224a5fd1a3a2684e0
Onatesla <73800811+Onatesla@users.noreply.github.com>
2022-03-21 15:42:47 +0100

    based

--- server.js
+++ server.js
@@ -3,20 +3,35 @@ const fs = require('fs');
 const express = require('express');
 const app =express();
 const port = process.env.port;
+const ipseDixit = 
+[
+ "...vile e scontata",
+ ", ma ti svegli?",
+ "? compare nu mongulu",
+ "... compare il mio tempo vale 600 euro ora e tu mi parli di sto cesso?",
+ "? Un sergio Placanica stupido e Sergio Placanica è stupido ",
+ " deve uscire dalla zona di comfort",
+ "? fratellino parliamo di soldi?",
+ "? nono non è bestia",
+ "? mi rissuru che futti",
+ "...mi inchiu u cazzu a pirita",
+ "..possiamo non parlare di evidenti mongoli?",
+ "..compare è autistico",
+ "...Front dev developer, what i said i told",
+
+
+]
+const value =  (Math.random() * (0+12) + 1);
 app.use(express.json())
 app.use(express.urlencoded())
 //routes
 app.post("/posta", (req,res)=> {
-   const  nome  = req.body.nome
-  console.log(nome)
-   if (!nome){
-     res.status(418).send({message: "Rammi stu nomi mongolo"})
-   }
-   res.send({ message: `io odio ${nome} e mi deve pure soldi` })
+ 
+
 })
 
 app.get("/", (req,res)=>{
-  res.send("idle...")
+  res.send({message: `${ipseDixit[value]}};
 })
 
 
